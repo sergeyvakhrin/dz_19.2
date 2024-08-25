4abc55435286f807b18cf980b98ef52da0468658
sergeyvakhrin <parlamentsv@mail.ru>
2024-08-25 14:24:54 +0300

    fix bug fill()

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
             if category['model'] == 'catalog.category':
                 category_for_create.append(Category(
                     category_name=data['category_name'],
-                    category_description=data.get('category_description'),
+                    category_description=data.get('category_description', None),
                     pk=category['pk']
                 ))
         Category.objects.bulk_create(category_for_create)
@@ -34,12 +34,13 @@ class Command(BaseCommand):
         for product in catalog:
             data = product['fields']
             if product['model'] == 'catalog.product':
+                valid_cat = Category.objects.get(pk=data.get('category')) if data['category'] else None
                 product_for_create.append(Product(
                     pk=product['pk'],
                     product_name=data['product_name'],
                     product_description=data['product_description'],
                     product_image=data['product_image'],
-                    category=Category.objects.get(pk=data['category']),
+                    category=valid_cat,
                     price=data['price'],
                     created_at=data['created_at'],
                     created_up=data['created_up'],
