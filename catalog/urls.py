59ce5b0dfee9d6b26425061f21e1a1defd6557ce
sergeyvakhrin <parlamentsv@mail.ru>
2024-09-01 17:06:44 +0300

    add contact detail templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list, contact_list, product_detail
+from catalog.views import product_list, contact_list, product_detail, contact_detail
 
 app_name = CatalogConfig.name
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', product_list, name='product_list'),
     path('contacts/', contact_list, name='contacts'),
     path('products/<int:pk>', product_detail, name='product_detail'),
+    path('contacts/<int:pk>', contact_detail, name='contact_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -25,3 +25,9 @@ def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'product': product}
     return render(request, 'product_detail.html', context)
+
+
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    context = {'contact': contact}
+    return render(request, 'contact_detail.html', context)
